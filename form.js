50633c5d48c77c29007bc7092ca66f1d95194823
devgrayhat <devgrayhat@gmail.com>
2023-03-06 22:47:02 +0500

    Added sendTransfer and error checks

--- form.js
+++ form.js
@@ -2,6 +2,12 @@ function inputValidation(inp){
     let fee
     let feePercentage
     const inputValue = parseFloat(inp.value)
+
+    if(inputValue>=0.005 && inputValue<=0.01){feePercentage = 0.03;}
+    else if(inputValue>0.01 && inputValue<=0.03){feePercentage = 0.0275;}
+    else if(inputValue>0.03 && inputValue<=0.05){feePercentage = 0.025;}
+
+    /*
     if(inputValue>=0.5 && inputValue<=1){feePercentage = 0.03;}
     else if(inputValue>1 && inputValue<=3){feePercentage = 0.0275;}
     else if(inputValue>3 && inputValue<=5){feePercentage = 0.025;}
@@ -9,11 +15,22 @@ function inputValidation(inp){
     //else if(inputValue>5 && inputValue<=10){feePercentage = 0.02;}
     //else if(inputValue>10 && inputValue<=20){feePercentage = 0.015;}
     //else if(inputValue>20){inputValue = 0.01;}
+    */
 
     fee = feePercentage*inputValue
     document.querySelector('.fee').innerHTML = fee.toFixed(5)
     document.querySelector('.totalValue').innerHTML = (inputValue + fee).toFixed(5)
-    document.querySelector(".eth-submit").innerHTML=`Transfer ${(inputValue + fee).toFixed(5)}`    
+    document.querySelector(".eth-submit").innerHTML=`Transfer ${(inputValue + fee).toFixed(5)}`
+    
+    const ethAddressRegex = /^(0x)?[0-9a-fA-F]{40}$/;
+    if(isNaN(fee))
+    {
+        document.getElementById("transferButton").disabled = true;
+        document.getElementById("transferButton").innerText = "Transfer";        
+    }    
+    else
+        document.getElementById("transferButton").disabled = false;
+    
 }
 
 function showForm(btn, mainDiv){
